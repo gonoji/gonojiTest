86cd1fd57631c036ae4889fa4db64a07816573f3
gonoji <kawarai.a.aa@m.titech.ac.jp>
2021-09-26 22:26:20 +0900

    Update poyo.js

--- gonojisGame/poyo.js
+++ gonojisGame/poyo.js
@@ -4,7 +4,7 @@ let Data = {};
 
 //Game Size
 Game.width = 800;
-Game.height = 400;
+Game.height = 450;
 
 //Title_Scene
 class Title_Scene extends Phaser.Scene {
@@ -61,23 +61,88 @@ class CantStop extends Phaser.Scene {
 
 	constructor() {
 		super({key: 'CantStop'});
+		let dicePict;
 	}
+	//let dicePict;
 	preload(){
 		this.load.spritesheet('dice', 'gonojisGame/assets/dice.png', { frameWidth: 32, frameHeight: 32 });
 	}
 
 	create() {
-		let text = this.add.text(0, 0, 'dededede').setFontSize(30);
-		text.setText('dododoodoa')
+		let textTitle = this.add.text(Game.width/16, Game.height/12, 'Cant Stop').setFontSize(30).setFontFamily("Arial");
+		let textTurn = this.add.text(Game.width/16, Game.height/5, '誰のターンです?').setFontSize(20).setFontFamily("Arial");
 		
-		const dices = this.physics.add.staticGroup({
-        	key: 'dice',
-       		repeat: 4,
-        	setXY: { x: Game.width/2, y: Game.width/4, stepX: 48 }
-    	});
+		let choices=[[[],[]],[[],[]],[[],[]]];
+		let row=0;
+		let column=0;
+		for(const choiceId in choices){
+			for(const pairId in choices[choiceId]){
+				choices[choiceId][pairId]=[];
+				const x0= pairId==0 ? Game.width/16 : Game.width/6;
+				const x1= x0+36;
+				const y = Game.height/2 + choiceId*48;
+				choices[choiceId][pairId][0]=this.add.sprite(x0,y,'dice').setOrigin(0,0);
+				choices[choiceId][pairId][1]=this.add.sprite(x1,y,'dice').setOrigin(0,0);
+			}
+		}
 
+
+
+		const dices = this.add.group({
+        	key: 'dice',
+       		repeat: 3,
+        	setXY: { x: Game.width/16, y: Game.height/3, stepX: 48 },
+        	//setXY: { x: Game.width/2, y: Game.height/4, stepX: 48 }
+    	})
+		
+		//text.setText('dododoodoa')
+		//text.setText('dododoodoa')
+		
+		
+    	dices.getChildren().forEach(dice=>dice.setOrigin(0,0))
+    	this.anims.create({
+        	key: 'rolling',
+        	frames: this.anims.generateFrameNumbers('dice', { start: 0, end: 5 }),
+        	frameRate: 10,
+        	repeat: -1
+    	});
+    	this.anims.create({
+    		key: 'face1',
+        	frames: [{key: 'duce', frame: 0}],
+        	frameRate: 10
+    	});
+    	this.anims.create({
+    		key: 'face2',
+        	frames: [{key: 'duce', frame: 1}],
+        	frameRate: 10
+    	});
+    	this.anims.create({
+    		key: 'face3',
+        	frames: [{key: 'duce', frame: 2}],
+    		frameRate: 10
+    	});
+    	this.anims.create({
+    		key: 'face4',
+        	frames: [{key: 'duce', frame: 3}],
+        	frameRate: 10,
+    	});
+    	this.anims.create({
+    		key: 'face5',
+        	frames: [{key: 'duce', frame: 4}],
+        	frameRate: 10,
+    	});
+    	this.anims.create({
+    		key: 'face6',
+        	frames: [{key: 'duce', frame: 5}],
+        	frameRate: 10,
+    	});
+    	dices.children.iterate(function (dice) {
+        	dice.anims.play('rolling',true);
+    	});
 	}
 	
+
+
 
 }
 
@@ -93,9 +158,6 @@ let config = {
     //    	mode: Phaser.Scale.FIT,
 	//	autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
 	//},
-	physics: {
-		default: 'arcade',
-	},
 	scene: [Title_Scene, Game_Scene, CantStop]
 };
 
